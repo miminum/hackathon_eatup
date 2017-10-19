a9a466d0741fe84830943078d60bb0401877a402
Daniel Huong <daniel.huong86@gmail.com>
2017-10-19 18:16:27 +1100

    merged with conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019040122) do
+ActiveRecord::Schema.define(version: 20171019040321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.text "blurb"
-    t.text "profile_image_data"
-    t.string "gender"
-    t.string "city"
-    t.string "country_code"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
@@ -43,6 +31,20 @@ ActiveRecord::Schema.define(version: 20171019040122) do
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "blurb"
+    t.text "profile_image_data"
+    t.string "gender"
+    t.string "city"
+    t.string "country_code"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
