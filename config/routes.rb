9d9ba6ad56bd6fa889490f3bbd0409ccc305da77
Jared Hutchinson <jared.hutchinson99@gmail.com>
2017-10-20 11:21:00 +1100

    Add messages index styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root :to => "profiles#index"
-  # root :to => "devise/sessions#new"
 
   resources :conversations do
     resources :messages
